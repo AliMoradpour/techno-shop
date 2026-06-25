87cb4ec4c5965f5650890f67dd32ab2ae1a28894
AliMoradpour <mr.mrdp@yahoo.com>
2026-06-26 00:20:53 +0330

    add limit to product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import data from "@/data/products";
 export default function HomePage() {
   return (
     <div>
-      <ProductList data={data.products} title="محصــولات" />
+      <ProductList data={data.products} title="محصــولات" limit={4} />
     </div>
   );
 }
