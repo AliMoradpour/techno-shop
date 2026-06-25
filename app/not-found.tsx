bb362b7b2891a8941e3e39f138119cfd7eb11d27
AliMoradpour <mr.mrdp@yahoo.com>
2026-06-26 00:02:49 +0330

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-2xl font-black">صفحه‌ی مورد نظر پیدا نشد</h2>
+      <Button variant="outline" className="mt-6" asChild>
+        <Link href="/">بازگشت به فروشگاه</Link>
+      </Button>
+    </div>
+  );
+}
