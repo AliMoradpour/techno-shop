bb7fe7dd261510ddced1d430d98f85adf7d2ffe6
AliMoradpour <mr.mrdp@yahoo.com>
2026-07-28 16:17:13 +0330

    otp form functions

--- app/(auth)/otp/OtpForm.tsx
+++ app/(auth)/otp/OtpForm.tsx
@@ -1,22 +1,69 @@
-'use client'
+"use client";
 
-import { findMobile } from "@/lib/actions/findMobile.actions"
-import { useState } from "react"
-
+import { signIn } from "@/auth";
+import { findMobile } from "@/lib/actions/findMobile.actions";
+import { OTP } from "@/lib/actions/otp.actions";
+import { FormEvent, useState } from "react";
 
 export default function OtpForm() {
- const [loginInProgress, setLoginInProgress] = useState(false) 
- const [isStepTwo, setIsStepTwo] = useState(false)
- const [mobile, setMobile] = useState('')
- const [userCode, setUserCode] = useState('')
- const [error, setError] = useState(false)
+  const [loginInProgress, setLoginInProgress] = useState(false);
+  const [isStepTwo, setIsStepTwo] = useState(false);
+  const [mobile, setMobile] = useState("");
+  const [sentCode, setSentCode] = useState("");
+  const [userCode, setUserCode] = useState("");
+  const [error, setError] = useState(false);
 
- async function checkMobile(phone: string){
-  setLoginInProgress(true)
-  const mobileNumber = await findMobile(phone)
- }
- 
- return (
-    <div>OtpForm</div>
-  )
+  async function checkMobile(phone: string) {
+    setLoginInProgress(true);
+    const mobileNumber = await findMobile(phone);
+
+    if (!mobileNumber) return null;
+
+    setIsStepTwo(true);
+
+    const otpCode = await OTP(phone);
+
+    setMobile(phone);
+
+    if (otpCode && otpCode.data && otpCode.data.code) {
+      setSentCode(otpCode.data.code);
+    } else {
+      setError(true);
+      setLoginInProgress(false);
+      return null;
+    }
+
+    setLoginInProgress(false);
+  }
+
+  async function verifyCode(code: string) {
+    const verification = sentCode == code;
+
+    if (!verification) {
+      setError(true);
+      return null;
+    }
+
+    await signIn("mobile-login", {
+      mobile: mobile,
+      callbackUrl: "/",
+    });
+  }
+
+  function submitHandler(event: FormEvent) {
+    event.preventDefault();
+
+    if (isStepTwo) {
+      verifyCode(userCode);
+    }
+    if (!isStepTwo) {
+      checkMobile(mobile);
+    }
+  }
+
+  return (
+    <>
+      <form onSubmit={submitHandler}></form>
+    </>
+  );
 }
